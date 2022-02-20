90af2f00a81a3263f4cca1daa93ffa93246e1aaf
Akshat <axxatagrawal@gmail.com>
2022-02-20 18:05:17 +0530

    commited

--- src/app.js
+++ src/app.js
@@ -6,15 +6,21 @@ const port = process.env.PORT || 3000;
 
 // path variable
 const staticpath = path.join(__dirname,"../public");
-
-
-// assigning hbs
+const temp_path = path.join(__dirname,"./templates/views")
+const partial_path = path.join(__dirname,"./templates/partials")
+//1 assigning hbs
 app.set('view engine','hbs');
+app.set('views',temp_path);
+hbs.registerPartials(partial_path);
+
 app.use(express.static(staticpath));
 
 
 // routing 
 app.get("/",(req,res)=>{
+    res.render("index")
+})
+app.get("/index",(req,res)=>{
     res.render("index")
 })
 app.get("/about",(req,res)=>{
@@ -23,7 +29,7 @@ app.get("/about",(req,res)=>{
 app.get("/weather",(req,res)=>{
     res.render("weather")
 })
-app.get("/*",(req,res)=>{
+app.get("*",(req,res)=>{
     res.render("404")
 })
 
